8c2446c44c19fda2ef003e39e56fe0a84f3709a7
pablohk <pablusmmid@gmail.com>
2017-11-21 17:39:50 +0100

    prepared to merge. Caution with seeds.js, not load some theaters

---
+++ routes/reviewController.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Review =require('../models/Review');
+
+
+router.get('/list', (req, res, next)=>{
+});
+
+router.get('/:id/detail', (req,res,next)=>{
+});
+
+
+
+module.exports = router;
