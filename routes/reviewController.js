e02a1a37361a5267fdf9c39c2b277e324d17aae8
pablohk <pablusmmid@gmail.com>
2017-11-22 19:45:36 +0100

    crud for review finish

--- routes/reviewController.js
+++ routes/reviewController.js
@@ -47,7 +47,7 @@ const EnsureOwnerReviewTheater = (req,res,next) =>{
   });
 };
 
-router.get('/newRevCine/:id', (req,res,next)=>{
+router.get('/newRevCine/:id',EnsureLoggedIn.EnsureLoggedIn, (req,res,next)=>{
   res.render('review/newRevCine',{cineId:req.params.id});
 });
 
@@ -63,7 +63,7 @@ router.post('/newRevCine/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewCi
       .catch( e => next (e));
 });
 
-router.get('/newRevTheater/:id', (req,res,next)=>{
+router.get('/newRevTheater/:id', EnsureLoggedIn.EnsureLoggedIn,(req,res,next)=>{
   res.render('review/newRevTheater',{theaterId:req.params.id});
 });
 
@@ -80,7 +80,7 @@ router.post('/newRevTheater/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerRevie
 });
 
 
-router.get('/listByUser', (req, res) => {
+router.get('/listByUser', [EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewTheater],(req, res) => {
   Review.find({'_user_id':{$eq:req.user._id}})
           .populate('_user_id')
           .populate('_cine_id')
@@ -91,53 +91,41 @@ router.get('/listByUser', (req, res) => {
           .catch(e =>{next(e);});
   });
 
+router.get('/edit/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewTheater],(req,res,next)=>{
+  const id=req.params.id;
+  Review.findById(id)
+    .then( item =>{
+      res.render('review/editReview', {item: item});})
+    .catch( e => {next (e);});
+  });
+
+
+router.post('/edit/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewTheater],(req,res,next)=>{
+  const id=req.params.id;
+  const {comment, rating }=req.body;
+  const update = {
+    comment,
+    rating,
+  };
+
+  console.log(update);
+  Review.findByIdAndUpdate(id,update)
+  .populate('_user_id')
+  .populate('_cine_id')
+  .populate('_theater_id')
+  .then((item)=>
+    {
+      console.log(item);
+      res.redirect('/review/listByUser');})
+    .catch(e=> next(e));
+});
+
+router.get('/delete/:id',EnsureLoggedIn.EnsureLoggedIn,(req,res,next)=>{
+  const id=req.params.id;
+  Review.findByIdAndRemove(id)
+  .then(()=>{res.redirect('/review/listByUser');})
+  .catch( e=> next(e));
+});
+
 
 module.exports = router;
-
-// router.post('/newRevCine/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewCine], (req,res,next)=>{
-//   const {comment,rating}=req.body;
-//   const cineId=req.params.id;
-//   const newRev = new Review ({
-//     comment,rating,_user_id:req.user._id,_cine_id:cineId});
-//
-//   newRev.save()
-//   .then(rev =>{
-//     Review.findById(rev._id)
-//       .populate('_user_id')
-//       .populate('_cine_id')
-//       .then(rev =>{
-//         res.render('review/reviewIndex',{rev});
-//       })
-//       .catch( e => next (e));
-//     })
-//   .catch(e=>{res.render('review/newRevCine');
-//   });
-// });
-//
-//
-//
-// router.get('/newRevTheater/:id', (req,res,next)=>{
-//   res.render('review/newRevTheater',{theaterId:req.params.id});
-// });
-
-
-
-// router.post('/newRevTheater/:id',[EnsureLoggedIn.EnsureLoggedIn,EnsureOwnerReviewTheater], (req,res,next)=>{
-//   const {comment,rating}=req.body;
-//   const theaterId=req.params.id;
-//   const newRev = new Review ({
-//     comment,rating,_user_id:req.user._id,_theater_id:theaterId});
-//
-//   newRev.save()
-//   .then(rev =>{
-//     Review.findById(rev._id)
-//       .populate('_user_id')
-//       .populate('_theater_id')
-//       .then(rev =>{
-//         res.render('review/reviewIndex',{rev});
-//       })
-//       .catch( e => next (e));
-//     })
-//   .catch(e=>{res.render('review/newRevTheater');
-//   });
-// });
