5c3056c9c515699b3718e38450140614f7dee817
pablohk <pablusmmid@gmail.com>
2017-11-23 13:10:42 +0100

    before merge thrusday at 13:15

--- routes/indexController.js
+++ routes/indexController.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/probes', function(req, res, next) {
+  res.render('probes/probes');
+});
+
+
 module.exports = router;
