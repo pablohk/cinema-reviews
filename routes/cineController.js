0ab165bbdf1df55e17421a6d95f98a5a96ca2c84
pablohk <pablusmmid@gmail.com>
2017-11-23 21:14:51 +0100

    final release beta 0

--- routes/cineController.js
+++ routes/cineController.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Cine =  require ('../models/Cine');
+const {ensureLoggedIn, ensureLoggedOut}= require('connect-ensure-login');
 
-router.get('/list', (req,res,next)=>{
+router.get('/list', ensureLoggedIn('/user/login'), (req,res,next)=>{
   Cine.find()
     .then(items=>{res.render('cine/cineList', {items});})
     .catch(err=>{res.render('cine/cineList',{error:err.message});
   });
 });
 
-router.get('/:id/detail', (req,res,next)=>{
+router.get('/:id/detail', ensureLoggedIn('/user/login'), (req,res,next)=>{
   const id =req.params.id;
   Cine.findById(id)
     .then(item=>{res.render('cine/cineDetail', {item});})
